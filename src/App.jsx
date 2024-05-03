bbe4d1f3e68305eae2e7a029e630807fc10cd980
Heta <hhmj28@gmail.com>
2024-05-04 00:22:38 +0300

    Koodia

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import { AppBar, Toolbar, Typography } from "@mui/material"
 
 function App() {
 
   return (
     <>
-    Mau
+    <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">Personal Training</Typography>
+        </Toolbar>
+      </AppBar>
     </>
   )
 }
